01a6ce43ddcf7dd7960403851b5954ffa20faa5f
kaseprekd <kasperekd@mail.ru>
2024-06-16 21:14:40 +0700

    connector main

--- connector/cmd/app/main.go
+++ connector/cmd/app/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"golang-coursework/connector/config"
+	"golang-coursework/connector/internal/app"
+	"golang-coursework/connector/pkg/logger"
+)
+
+func main() {
+	configPath := flag.String("configPath", "connector/config/config-connector.yaml", "Path to the config file")
+	flag.Parse()
+
+	log := logger.CreateNewLogger()
+
+	cfg, err := config.NewConfig(*configPath)
+
+	if err != nil {
+		log.Log(logger.ERROR, err.Error())
+		panic(err)
+	}
+
+	newApp, err := app.NewApp(cfg, log)
+
+	if err != nil {
+		log.Log(logger.ERROR, err.Error())
+		panic(err)
+	}
+
+	defer func(newApp *app.App) {
+		err := newApp.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+
+			log.Log(logger.ERROR, err.Error())
+			panic(err)
+		}
+	}(newApp)
+
+	if err = newApp.Run(); err != nil {
+		log.Log(logger.ERROR, err.Error())
+		panic(err)
+	}
+}
